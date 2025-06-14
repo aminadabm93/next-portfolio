05a76eee2100a828f13f797fae73883abf7d02ad
aminadab.morales <aminadabm93@gmail.com>
2025-06-14 15:52:15 -0400

    removed unused compts

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,6 @@ export default function Home() {
     <section className='pt-[20%]'>
       <div className='container max-w-4xl'>
         <Intro />
-
-        {/* <RecentPosts />
-        <RecentProjects /> */}
-        {/* maybe show something in desktop*/}
-        {/* <NewsletterForm /> */}
       </div>
     </section>
   )
