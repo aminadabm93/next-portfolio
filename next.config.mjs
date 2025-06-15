bc8e1f97dfeec529d422c957daf9fab812b92ece
aminadab.morales <aminadabm93@gmail.com>
2025-06-15 00:45:01 -0400

    new config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-const { withCloudflarePagesAdapter } = require('@cloudflare/next-on-pages/adapter');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
@@ -7,10 +6,3 @@ if (process.env.NODE_ENV === 'development') {
     await setupDevPlatform();
   }
 export default nextConfig;
-
-module.exports = withCloudflarePagesAdapter({
-  // Your existing Next.js configuration
-  experimental: {
-    appDir: true, // Enable if you're using the app directory
-  },
-});
